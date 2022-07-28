4ead55e0152673560eb464ccd099258ccc74f29c
Bogdan Andreiana <bogdanandreiana13@gmail.com>
2022-07-28 14:10:28 +0300

    Added auto fullscreen to webgl build

--- Assets/Scripts/ReloadCloseOnKey.cs
+++ Assets/Scripts/ReloadCloseOnKey.cs
@@ -14,14 +14,27 @@ public class ReloadCloseOnKey : MonoBehaviour
 
 	private void Update()
     {
-		if (curKeyboard.escapeKey.isPressed)
-		{
-			Application.Quit();
-		}
-
-		if (curKeyboard.rKey.isPressed)
+	    if (curKeyboard.escapeKey.isPressed)
+	    {
+#if !UNITY_WEBGL
+		    Application.Quit();
+#endif
+		    Screen.fullScreen = !Screen.fullScreen;
+	    }
+	    if (curKeyboard.rKey.isPressed)
 		{
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 		}
+
+#if UNITY_WEBGL
+		if (Input.GetMouseButton(0))
+	    {
+		    // Get highest resolution supported by the current screen. 
+		    var resolution = Screen.resolutions[Screen.resolutions.Length - 1];
+
+		    // The last parameter "true" denotes if it should be fullscreen or not. 
+		    Screen.SetResolution(resolution.width, resolution.height, true);
+	    }
+#endif
 	}
 }
